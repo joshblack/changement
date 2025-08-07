7ea9c0e7fa8c395ccac8a4165b0e5a4de7c4970f
Josh Black <joshblack@github.com>
2025-08-06 21:14:31 -0500

    chore: run format

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use anyhow::Result;
 use clap::{Parser, Subcommand};
 use std::fs;
 use std::path::Path;
-use anyhow::Result;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ChangementError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    
+
     #[error("Configuration error: {0}")]
     Config(String),
 }
@@ -82,7 +82,7 @@ mod tests {
     fn test_changement_error_display() {
         let error = ChangementError::Config("test config error".to_string());
         assert_eq!(error.to_string(), "Configuration error: test config error");
-        
+
         let io_error = std::io::Error::new(std::io::ErrorKind::NotFound, "file not found");
         let error = ChangementError::Io(io_error);
         assert_eq!(error.to_string(), "IO error: file not found");
